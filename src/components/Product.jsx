33ccb6257bb2df4514ecda1eda4d70365447a1f9
YulicethGonzalezC <yuliceth.gonzalezc@gmail.com>
2023-06-14 00:26:57 -0500

    agregando consumo de api para visualizar productos

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,45 @@
-function Product() {
-    return <div>
-          
-    </div>;
-  }
-  
-  export default Product;
-  
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+
+export default function Product (){
+    const [loading, setLoading] = useState(false);
+    const[data, setData]= useState([]);
+
+    useEffect(()=>{
+        setLoading(true)
+       axios(
+        {
+            method:"GET",
+            url:"https://fakestoreapi.com/products"
+        }).then(res=>{
+            console.log(res.data)
+            setData(res.data)
+        }).catch(e=>console.log(e))
+        .finally(()=>setLoading(false));
+    },[]);
+
+
+
+    return(
+        <div className ="products-Container">
+            {loading && (
+                <div>
+                    {""}
+                    <h1>Loading...</h1>
+                </div>
+            )}
+     
+       {data.map((product)=>(
+        <div key={product.id}>
+            <div><img src={product.image} alt="#"/> </div>
+            <div className="card-description">  </div>
+           <h6>{product.title}</h6>
+           <h6>{`Precio:${product.price}`}</h6>
+           <h6>{`Categoria:${product.category}`}</h6>
+        </div>
+       )
+        )}
+    </div>
+    );
+}
